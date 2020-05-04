19f372a06d854f8b97fd82622189c0d747873237
m3rt32 <mertkirimgeri@gmail.com>
2020-05-04 05:48:19 +0300

    Folder re-organization, Scene included

---
+++ Scripts/DemoObjectTranslation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoObjectTranslation : MonoBehaviour
+{
+    [SerializeField] private float speed = 2;
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal")*Time.deltaTime*speed, transform.position.y,
+            transform.position.z + Input.GetAxis("Vertical")*Time.deltaTime*speed);
+        ;
+    }
+}
